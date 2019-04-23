83006915a3b0f23916f3e8572d37599250b92cee
rudtyz <rudtyz@gmail.com>
2019-04-24 00:34:40 +0900

    chage dflat impl

--- prelude.js
+++ prelude.js
@@ -84,21 +84,22 @@ const isChar = a => (a.constructor === String && a.length <= 1);
 
 const dflat = async function* (...iters) {
     for await (const it of iters) {
-        if (!it) {
-            yield it;
-        }
-        if (isChar(it)) {
-            // if without this code dflat(new String('a')) 
-            // returns [new String('a')]
-            // require ['a']
-            yield it.valueOf();
-        } else if (it[Symbol.asyncIterator] || it[Symbol.iterator]) {
-            for await (const e of it) {
-                yield* dflat(e);
+        if (it) {
+            if (isChar(it)) {
+                // if without this code dflat(new String('a')) 
+                // returns [new String('a')]
+                // require ['a']
+                yield it.valueOf();
+                continue;
+            } else if (it[Symbol.asyncIterator] || it[Symbol.iterator]) {
+                for await (const e of it) {
+                    yield* dflat(e);
+                }
+                continue;
             }
-        } else {
-            yield it;
         }
+        yield it;
+
     }
 };
 
