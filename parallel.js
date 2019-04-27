1849de00606673545a658ed17fbfdad7ec82085a
석경우 <rudtyz@gmail.com>
2019-04-27 15:02:54 +0900

    change internal func name

--- parallel.js
+++ parallel.js
@@ -11,8 +11,8 @@ exports.parallel_set_fetch_count = (count) => {
     global_fetch_count = count || default_fetch_count;
 };
 
-const fetch_iterator = async (f, fn, iter) => {
-    const fetch_count = global_fetch_count; 
+const fetch_map_iterator = async (f, fn, iter) => {
+    const fetch_count = global_fetch_count - 1; 
     const g = iter[Symbol.iterator]();
     for (let i = fetch_count; i > 0; --i) {
         const e = g.next();
@@ -24,8 +24,8 @@ const fetch_iterator = async (f, fn, iter) => {
     return g;
 };
 
-const fetch_asyncIterator = async (f, fn, iter) => {
-    const fetch_count = global_fetch_count; 
+const fetch_map_asyncIterator = async (f, fn, iter) => {
+    const fetch_count = global_fetch_count - 1; 
     const g = iter[Symbol.asyncIterator]();
     for (let i = fetch_count; i > 0; --i) {
         const e = await g.next();
@@ -37,36 +37,33 @@ const fetch_asyncIterator = async (f, fn, iter) => {
     return g;
 };
 
-const fetch_internal = (f, fn, iter) => {
+const fetch_map_internal = (f, fn, iter) => {
     if (iter[Symbol.asyncIterator]) {
-        return fetch_asyncIterator(f, fn, iter);
+        return fetch_map_asyncIterator(f, fn, iter);
     }
-    return fetch_iterator(f, fn, iter);
+    return fetch_map_iterator(f, fn, iter);
 };
 
 exports.pmap = C.curry(async function* (fn, iter) {
     const f = [];
-    const g = await fetch_internal(f, fn, iter);
+    const g = await fetch_map_internal(f, fn, iter);
+
     for await (const e of g) {
-        yield await f.shift();
         f.push(fn(e));
+        yield await f.shift();
     }
 
     yield* f;
 });
 
 const pfmap = C.curry(async function* (fn, iter) {
-    const fetch_count = global_fetch_count;
 
     let f = [];
-    for await (const e of iter) {
-        const len = f.push(fn(e));
-        if(len >= fetch_count) {
-            for (let i = 0; i < f.length; ++i) {
-                yield* await f[i];
-            }
-            f = [];
-        }
+    const g = await fetch_map_internal(f, fn, iter);
+
+    for await (const e of g) {
+        f.push(fn(e));
+        yield* await f.shift();
     }
 
     for (let i = 0; i < f.length; ++i) {
