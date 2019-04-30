ed58e165b25e6df852966dd28d7a79ac74eb75f6
rudtyz <rudtyz@gmail.com>
2019-04-30 23:30:03 +0900

    pipe add test

--- test/pipe.spec.js
+++ test/pipe.spec.js
@@ -4,11 +4,31 @@ const assert = require("assert");
 
 describe('test pipe', () => {
     it('default', async () => {
-        const c = F.compose(
+        const c = F.pipe(
             F.map(e => e + 1),
             F.filter(e => e % 2 == 0),
             F.collect);
-        // assert.deepStrictEqual(await F.collect(c), [1,2,3,4,5,6]);
-        console.log(await c([1,2,3,4,5]))
+        assert.deepStrictEqual(await c([1,2,3,4,5]), [2,4,6]);
+        assert.deepStrictEqual(await c([1,2,3,4,5]), [2,4,6]);
+        assert.deepStrictEqual(await c([1,2,3,4,5]), [2,4,6]);
+        assert.deepStrictEqual(await c([1,2,3,4,5]), [2,4,6]);
+        assert.deepStrictEqual(await c([1,2,3,4,5]), [2,4,6]);
+    });
+
+    it('return undefined', async () => {
+        const c = F.pipe(
+            F.map(e => e + 1),
+            F.filter(e => e % 2 == 0),
+            ()=>{});
+
+
+        assert.strictEqual(await c([0]), undefined);
+        assert.strictEqual(await c([1]), undefined);
+        assert.strictEqual(await c([0,1]), undefined);
+    });
+
+    it('toString', async () => {
+        const c = F.pipe(()=>{});
+        assert.equal(c.toString().length > 0, true);
     });
 });
