36dbe1f6d65e0250dac0c30e66b0577e51c10064
Lee Tae Woo <lee111107@gmail.com>
2019-05-04 16:23:26 +0900

    add expect error type test

---
+++ test/type/lisp.error.ts
@@ -0,0 +1,15 @@
+import * as F from '../../';
+
+type DoneFn = () => any;
+
+declare function describe(s: string, f: () => any): void;
+declare function it(s: string, f: (done: DoneFn) => any): void;
+
+const thanos = () => Math.random() >= 0.5;
+
+describe('lisp', () => {
+    it('error', async () => {
+        const e0 = await F.cond(thanos); // $ExpectError
+        const e1 = await F.cond(thanos, 1, 'a'); // $ExpectError
+    });
+});
