64fc8ab3c10d78b5c9ff3339e5773fd912e0e6d9
Lee Tae Woo <syr@meu.works>
2019-05-05 20:00:56 +0900

    edit pflatMap type test

--- test/type/parallel.spec.ts
+++ test/type/parallel.spec.ts
@@ -212,10 +212,13 @@ describe('pflatMap', () => {
 	});
 
     it('with run', async () => {
-        const a = [[1], Promise.resolve(['a']), [4], [5]];
+        const a = [[1], Promise.resolve(['a']), [4], [5], [Promise.resolve('b')]];
         const b = [[1], Promise.resolve([2]), Promise.resolve([3]), [4], [5]];
 
-        const ar0 = await F.run(a, e0 => F.pflatMap(e1 => e1, e0)); // $ExpectType AsyncIterableIterator<string | number>
+        const ar0 = await F.run(a, F.pflatMap(e => { // $ExpectType AsyncIterableIterator<string | number>
+            e; // $ExpectType string[] | number[] | Promise<string>[]
+            return e;
+        }));
 
         const br0 = await F.run(b, F.pflatMap(e => e)); // $ExpectType AsyncIterableIterator<number>
     });
