79fb64ae998449da577c2ddae10a4c5bf62cc453
석경우 <rudtyz@gmail.com>
2019-05-06 15:05:14 +0900

    count support object & length & size

--- stream.js
+++ stream.js
@@ -125,11 +125,27 @@ const minBy = C.curry(async (f, iter) => {
 exports.minBy = minBy;
 
 exports.count = async (iter) => {
-    let c = 0;
-    for await (const _ of iter) {
-        ++c;
+    //array, string
+    if (iter.length && Number.isInteger(iter.length)) {
+        return iter.length;
     }
-    return c;
+
+    //map, set, any collection
+    if (iter.size && Number.isInteger(iter.size)) {
+        return iter.size;
+    }
+
+    //iterators
+    if (iter[Symbol.asyncIterator] || iter[Symbol.iterator]) {
+        let c = 0;
+        for await (const _ of iter) {
+            ++c;
+        }
+        return c;
+    }
+
+    //object
+    return Object.keys(iter).length;
 };
 
 exports.sum = P.foldl1(C.add);
--- test/count.spec.js
+++ test/count.spec.js
@@ -15,4 +15,41 @@ describe('test count', () => {
         assert.strictEqual(n, 5);
     });
 
+    it('iter', async () => {
+        const a = "abcde"[Symbol.iterator](); 
+        const n = await F.count(a);
+        assert.strictEqual(n, 5);
+    });
+
+    it('iter', async () => {
+        const a = (async function*() {
+            yield* "abcde";
+        })();
+        const n = await F.count(a);
+        assert.strictEqual(n, 5);
+    });
+
+    it('map', async () => {
+        const a = new Map([
+            ["A",1],
+            ["B",2],
+            ["C",3],
+            ["D",4],
+            ["E",5]
+        ]);
+        const n = await F.count(a);
+        assert.strictEqual(n, 5);
+    });
+
+    it('object', async () => {
+        const a = {
+            "A":1,
+            "B":2,
+            "C":3,
+            "D":4,
+            "E":5
+        }; 
+        const n = await F.count(a);
+        assert.strictEqual(n, 5);
+    });
 });
