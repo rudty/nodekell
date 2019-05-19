420b285e2405f5a25eb7f5c83e8a18ed497322fb
석경우 <rudtyz@gmail.com>
2019-05-19 16:55:50 +0900

    add zip test

--- test/zip3.spec.js
+++ test/zip3.spec.js
@@ -1,5 +1,5 @@
 "use strict";
-const F = require("../prelude");
+const F = require("../index.js");
 const assert = require("assert");
 
 describe('test zip3', () => {
@@ -8,10 +8,34 @@ describe('test zip3', () => {
         const b = [4,5,6];
         const c = [7,8,9];
         const r = F.zip3(a,b,c);
-        const result = []
-        for await (const e of r) {
-           result.push(e); 
-        }
+        const result = await F.collect(r);
         assert.deepStrictEqual(result, [[1,4,7],[2,5,8],[3,6,9]]);
+    });
+
+    it('array 233', async () => {
+        const a = [1,2];
+        const b = [4,5,6];
+        const c = [7,8,9];
+        const r = F.zip3(a,b,c);
+        const result = await F.collect(r);
+        assert.deepStrictEqual(result, [[1,4,7],[2,5,8]]);
+    });
+
+    it('array 323', async () => {
+        const a = [1,2,3];
+        const b = [4,5];
+        const c = [7,8,9];
+        const r = F.zip3(a,b,c);
+        const result = await F.collect(r);
+        assert.deepStrictEqual(result, [[1,4,7],[2,5,8]]);
+    });
+
+    it('array 332', async () => {
+        const a = [1,2,3];
+        const b = [4,5,6];
+        const c = [7,8];
+        const r = F.zip3(a,b,c);
+        const result = await F.collect(r);
+        assert.deepStrictEqual(result, [[1,4,7],[2,5,8]]);
     });
 });
