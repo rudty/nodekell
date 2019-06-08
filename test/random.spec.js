7844b9b996df676796d74dc6823be6e8130b488d
석경우 <rudtyz@gmail.com>
2019-06-08 19:56:37 +0900

    change random impl

--- test/random.spec.js
+++ test/random.spec.js
@@ -4,50 +4,50 @@ const assert = require("assert");
 describe('test random', () => {
     it('end 1byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(255)),
+            F.repeat(10000, () => F.random(255)),
             F.forEach(e => assert.ok(e >= 0 && e < 255))
         );
     });
    
     it('end 1byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(255)),
+            F.repeat(10000, () => F.random(255)),
             F.forEach(e => assert.ok(e >= 0 && e < 255))
         );
     });
     it('end 2byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(65531)),
+            F.repeat(10000, () => F.random(65531)),
             F.forEach(e => assert.ok(e >= 0 && e < 65531))
         );
     });
     it('end 3byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(16777213)),
+            F.repeat(10000, () => F.random(16777213)),
             F.forEach(e => assert.ok(e >= 0 && e < 16777213))
         );
     });
     it('end 4byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(16777219)),
+            F.repeat(10000, () => F.random(16777219)),
             F.forEach(e => assert.ok(e >= 0 && e < 16777219))
         );
     });
     it('begin-end 1byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(8, 50)),
+            F.repeat(10000, () => F.random(8, 50)),
             F.forEach(e => assert.ok(e >= 8 && e < 50))
         );
     });
     it('begin-end 1byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(8, 50)),
+            F.repeat(10000, () => F.random(8, 50)),
             F.forEach(e => assert.ok(e >= 8 && e < 50))
         );
     });
     it('begin-end 4byte', async () => {
         await F.run(
-            F.repeat(100, () => F.random(8, 16777219)),
+            F.repeat(10000, () => F.random(8, 16777219)),
             F.forEach(e => assert.ok(e >= 8 && e < 16777219))
         );
     });
@@ -58,6 +58,8 @@ describe('test random', () => {
         );
     });
     it('0', async () => {
-        assert.strictEqual(F.random(0, 1), 0);
+        for (let i = 0; i < 10000; ++i){
+            assert.strictEqual(F.random(0, 1), 0);
+        }
     });
 });
