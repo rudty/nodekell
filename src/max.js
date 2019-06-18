f603e514df3307489ec28a3c050e05e88f427fd2
석경우 <rudtyz@gmail.com>
2019-06-18 23:27:27 +0900

    add comment

--- src/max.js
+++ src/max.js
@@ -1,4 +1,11 @@
 import { identity } from "./identity"
 import { maxBy } from "./maxBy";
 
+/**
+ * Gets the max value in the range
+ *
+ * await F.max([1,2,3,4,5]);
+ * 
+ * => 5
+ */
 export const max = maxBy(identity);
--- src/min.js
+++ src/min.js
@@ -1,4 +1,10 @@
 import { identity } from "./identity"
 import { minBy } from "./minBy";
-
+/**
+ * Gets the min value in the range
+ *
+ * await F.min([1,2,3,4,5,0]);
+ * 
+ * => 0
+ */
 export const min = minBy(identity);
