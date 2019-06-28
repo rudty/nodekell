70e0b1374dc3071815a8c51940e808f6007588c9
석경우 <rudtyz@gmail.com>
2019-06-28 17:24:07 +0900

    change repeat impl

--- src/repeat.js
+++ src/repeat.js
@@ -1,18 +1,43 @@
-export const repeat = async function *(a, ...b) {
-    let supply = a;
-    let len = Infinity;
-    if (b.length > 0) {
-        supply = b[0];
-        len = await a;
+const repeatWithFunction = async function *(len, supply) {
+    for (let i = len; i > 0; --i) {
+        yield supply();
     }
-    supply = await supply;
+};
+
+const repeatWithValue = async function *(len, supply) {
+    for (let i = len; i > 0; --i) {
+        yield supply;
+    }
+};
+
+/**
+ * arity 1 : [Infinity, arg1]
+ * arity 2 : [arg1, arg2]
+ */
+const repeatFetchArgument = async (a, b) => {
+    a = await a;
+    if (b.length > 0) {
+        return [a, await b[0]];
+    }
+    return [Infinity, a];
+};
+
+/**
+ * supply
+ * F.repeat(5) => [5,5,5,....]
+ * 
+ * count and supply
+ * F.repeat(3, 5) => [5,5,5]
+ * 
+ * 
+ * @param {Number | any} a  count or supply
+ * @param  {any?} b supply
+ */
+export const repeat = async function *(a, ...b) {
+    const [len, supply] = await repeatFetchArgument(a, b);
     if (supply instanceof Function) {
-        for (let i = len; i > 0; --i) {
-            yield await supply();
-        }
+        yield* repeatWithFunction(len, supply);
     } else {
-        for (let i = len; i > 0; --i) {
-            yield supply;
-        }
+        yield* repeatWithValue(len, supply);
     }
 };
