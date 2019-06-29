86996a78a91a710ffe75a5c92c2ad98c21891ba3
석경우 <rudtyz@gmail.com>
2019-06-29 23:56:29 +0900

    add test collectNative

--- test/collectNative.spec.js
+++ test/collectNative.spec.js
@@ -10,46 +10,54 @@ describe('test collectNative', () => {
     it('collectInt8', async () => {
         const r = await F.collectInt8([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Int8Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Int8Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectInt16', async () => {
         const r = await F.collectInt16([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Int16Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Int16Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectInt32', async () => {
         const r = await F.collectInt32([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectUint8', async () => {
         const r = await F.collectUint8([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Uint8Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Uint8Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectUint16', async () => {
         const r = await F.collectUint16([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Uint16Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Uint16Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectUint32', async () => {
         const r = await F.collectUint32([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Uint32Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Uint32Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectUint8Clamped', async () => {
         const r = await F.collectUint8Clamped([1,2,3,4,5]);
-        assert.deepStrictEqual(arrayOf(Uint8ClampedArray)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Uint8ClampedArray)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectFloat32', async () => {
         const r = await F.collectFloat32([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Float32Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Float32Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('collectFloat64', async () => {
         const r = await F.collectFloat64([1,2,3,4,5]);
         assert.deepStrictEqual(arrayOf(Float64Array)([1,2,3,4,5]),r);
+        assert.deepStrictEqual(arrayOf(Float64Array)([1,2,3,4,5]).byteLength,r.byteLength);
     });
 
     it('generator', async () => {
@@ -61,6 +69,7 @@ describe('test collectNative', () => {
 
         const r = await F.collectInt32(g());
         assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]),r);
+        assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]).byteLength,r.byteLength);
     });
 
     it('async generator', async () => {
@@ -72,6 +81,7 @@ describe('test collectNative', () => {
 
         const r = await F.collectInt32(g());
         assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]),r);
+        assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]).byteLength,r.byteLength);
     });
 
     it('async generator', async () => {
@@ -83,10 +93,12 @@ describe('test collectNative', () => {
 
         const r = await F.collectInt32(g());
         assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]),r);
+        assert.deepStrictEqual(arrayOf(Int32Array)([1,2,3]).byteLength,r.byteLength);
     });
 
     it('string?', async () => {
         const r = await F.collectInt32(["hello","world"]);
         assert.deepStrictEqual(arrayOf(Int32Array)([0, 0]),r);
+        assert.deepStrictEqual(arrayOf(Int32Array)([0, 0]).byteLength,r.byteLength);
     });
 });
