97df4db9fb11d75ecba40f39e7112436fc2aaa18
석경우 <rudtyz@gmail.com>
2019-07-09 21:07:01 +0900

    remove console.log

--- index.js
+++ index.js
@@ -1239,10 +1239,7 @@ const parallel_set_fetch_count_internal = (count) => {
 const parallel_fetch_map_internal = async (iter, fn) => {
     // fetch (n - 1) here
     const fetchCount = global_fetch_count - 1;
-    console.log(iter.constructor);
     const g = seq(iter);
-    console.log(g);
-    console.log(g.next);
     for (let i = fetchCount; i > 0; --i) {
         const e = await g.next();
         if (e.done) {
