63f877a474a5a13eeb8b961f4aff064c15bbedab
석경우 <rudtyz@gmail.com>
2019-07-30 20:48:42 +0900

    remove takeif

--- src/internal/runtime.js
+++
@@ -1,4 +0,0 @@
-export const returnZero = () => 0;
-export const returnNilString = () => "nil";
-export const returnEmptyIterator = function *() { };
-export const returnEmptyAsyncIterator = async function *() { };
