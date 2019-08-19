4b6c24a93f56369d4be9e94995903a4ce8843c9b
석경우 <rudtyz@gmail.com>
2019-08-19 20:46:00 +0900

    remove warn

--- src/reFindAll.js
+++ src/reFindAll.js
@@ -14,6 +14,6 @@ import { findAllSubMatch } from "./internal/regex";
  */
 export const reFindAll = curry((re, str) => {
     const r = [];
-    findAllSubMatch(re, str, e => r.push(e[0]));
+    findAllSubMatch(re, str, (e) => r.push(e[0]));
     return r;
 });
