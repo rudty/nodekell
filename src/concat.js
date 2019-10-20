54575c2c306cacf799f15d49b8a202929879c2c5
석경우 <rudtyz@gmail.com>
2019-10-20 18:39:59 +0900

    insertAt

--- src/concat.js
+++ src/concat.js
@@ -2,7 +2,7 @@ import { curry } from "./curry";
 import { flatOnce } from "./internal/flatOnce";
 
 export const concat = curry(async function *(a, b) {
-    yield* a;
+    yield* flatOnce(a);
     yield* flatOnce(b);
 });
 export const union = concat;
