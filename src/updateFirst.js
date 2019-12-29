8ddfa5e2834ae8852d7588323616477c8cae0aca
석경우 <rudtyz@gmail.com>
2019-12-29 20:28:15 +0900

    updateFirstFunction add value

--- src/updateFirst.js
+++ src/updateFirst.js
@@ -1,20 +1,29 @@
 import { _isFunction } from "./internal/typeTraits";
 import { equals } from "./equals";
 
-const _updateFirstFunction = async function *() {
-
+const _updateFirstFunction = async function *(value, comp, iter) {
+    const g = seq(iter);
+    for await (const e of g) {
+        if (await comp(e)) {
+            yield value;
+            yield* g;
+            return;
+        } else {
+            yield e;
+        }
+    }
 };
 
 /**
  * @param {*} x predicate. update value or find function
  * @param {Iterable | AsyncIterable} iter any iterable
  */
-export const updateFirst = curry(async function *(x, iter) {
+export const updateFirst = curry(async function *(value, x, iter) {
     x = await x;
     if (_isFunction(x)) {
-        yield* _updateFirstFunction(x, iter);
+        yield* _updateFirstFunction(value, x, iter);
     } else {
         const compareFunction = equals(x);
-        yield* _updateFirstFunction(compareFunction, iter);
+        yield* _updateFirstFunction(value, compareFunction, iter);
     }
 });
