5b3ca5028f9abaaa6d7b87a98d0e8a80e326d9ab
석경우 <rudtyz@gmail.com>
2020-01-13 21:33:12 +0900

    reverse

--- src/reverse.ts
+++ src/reverse.ts
@@ -3,6 +3,15 @@ import { Iter, ExtractPromise } from "./internal/typeTraits";
 export interface Reverse {
     <T>(iter: Iter<T>): AsyncIterableIterator<ExtractPromise<T>>;
 }
+
+/**
+ * reverse iterator
+ * @example
+ * const a = [1,2,3,4,5];
+ * const t = F.reverse(a);
+ * console.log(await F.collect(t)); // print 5,4,3,2,1
+ * @param iter any iterator
+ */
 export const reverse: Reverse = async function *(iter: Iter<any>): AsyncIterableIterator<any> {
     const a = await _collectArray(iter);
     for (let i = a.length - 1; i >= 0; i -= 1) {
