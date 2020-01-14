7811c0aab69c055a2d8b6a480ff064542c76bece
석경우 <rudtyz@gmail.com>
2020-01-14 21:27:17 +0900

    _

---
+++ src/_.ts
@@ -0,0 +1,13 @@
+/**
+ * empty object
+ * it is always return true when used as an argument in `equals` and `match` function
+ * @example
+ * F.equals(1, F._); // true
+ * F.equals({}, F._); // true
+ * F.equals({ a: 1 }, { a: F._ }); //true
+ *
+ * {} === F._; // false
+ * 1 === F._; // false
+ */
+export const underBar: any = Object.freeze({});
+export const _: any = underBar;
