b836f9e95f845fc80893d9e3c09287c5604d5a97
석경우 <rudtyz@gmail.com>
2020-01-14 19:59:18 +0900

    inc move comment

--- src/inc.ts
+++ src/inc.ts
@@ -1,3 +1,12 @@
+export interface Inc {
+    (a: null): number;
+    (a: undefined): number;
+    (a: number): number;
+    (a: string): string;
+    (a: Array<any>): string;
+    (a: any): any;
+}
+
 /**
  * @example
  * inc([1,2,3]);            // '1,2,31'
@@ -8,13 +17,6 @@
  * inc(new Int32Array(1));  // "01"
  * inc("hello");            // "hello1"
  * @param a any object
+ * @returns a + 1
  */
-export interface Inc {
-    (a: null): number;
-    (a: undefined): number;
-    (a: number): number;
-    (a: string): string;
-    (a: Array<any>): string;
-    (a: any): any;
-}
 export const inc: Inc = (a: any): any => a + 1;
