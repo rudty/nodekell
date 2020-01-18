3845920e5ba66b837083065491e49e5fbbc3ad66
석경우 <rudtyz@gmail.com>
2020-01-18 11:31:25 +0900

    collectMap

---
+++ src/collectMap.ts
@@ -0,0 +1,23 @@
+import { _collectArray } from "./internal/collectArray";
+import { Iter } from "./internal/typeTraits";
+
+export interface CollectMap {
+    /**
+     * @example
+     * const a = [[1,2],[3,4]];
+     * const m = await F.collectMap(a); // new Map([[1,2],[3,4]])
+     * for(const [k,v] of m) {
+     *     console.log(k, v);
+     * }
+     * // print 
+     * // 1 2
+     * // 3 4
+     *
+     * // if want you high quality type, use type assertion
+     * const a = [['a', 0], ['b', 1]] as [string, number][];
+     * const r = await collectMap(a); // Map<string, number>
+     * @param iter any iterator
+     */
+    <T extends any[]>(iter: Iter<T | Promise<T>>): Promise<Map<T[0], T[1]>>;
+}
+export const collectMap: CollectMap = async (iter: Iter<any>) => new Map((<[any, any]>(await _collectArray(iter))));
