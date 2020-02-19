c0ab7e7315188889ed3cab1fbfd3fe3e78db7d10
석경우 <rudtyz@gmail.com>
2020-02-19 22:36:49 +0900

    Map => MapType

--- src/map.ts
+++ src/map.ts
@@ -1,14 +1,14 @@
 import { curry } from "./curry";
 import { Iter, _Func1, ExtractPromise, _FlatFunc1 } from "./internal/typeTraits";
 
-export interface Map {
+export interface MapType {
     <T, R>(f: _Func1<T, R>, iter: Iter<T | Promise<T>>): AsyncIterableIterator<R>;
     <T extends Iter<any>, R>(f: _FlatFunc1<T, R>, iter: T): AsyncIterableIterator<ExtractPromise<R>>;
     <T extends Iter<any>, R>(f: _FlatFunc1<T, R>): (iter: T) => AsyncIterableIterator<ExtractPromise<R>>;
     <T, R>(f: _Func1<T, R>): (iter: Iter<T | Promise<T>>) => AsyncIterableIterator<R>;
 }
 
-export const map: Map = curry(async function *(fn: any, iter: Iter<any>): AsyncIterableIterator<any> {
+export const map: MapType = curry(async function *(fn: any, iter: Iter<any>): AsyncIterableIterator<any> {
     for await (const e of iter) {
         yield fn(e);
     }
