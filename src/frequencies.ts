0cb9643a02870d6f3c24a52f4dcf68df10d057ce
석경우 <rudtyz@gmail.com>
2020-03-24 19:14:36 +0900

    frequencies

---
+++ src/frequencies.ts
@@ -0,0 +1,19 @@
+import { identity } from "./identity";
+import { frequenciesBy } from "./frequenciesBy";
+import { Iter, FlatForInternalFn } from "./internal/typeTraits";
+
+export interface FrequenciesType {
+    /**
+     * returns a Map. keys are the items that remove duplicates value is the number of times key 
+     * @example
+     * const arr = [1,1,2,3,4,5,5];
+     * const r = await F.frequencies(arr);
+     * console.log(r);
+     * // print
+     * // Map { 1 => 2, 2 => 1, 3 => 1, 4 => 1, 5 => 2 }
+     * @param iter any iterator
+     */
+    <T extends Iter<any>>(iter: T): Promise<Map<FlatForInternalFn<T>, number>>;
+}
+
+export const frequencies: FrequenciesType = frequenciesBy(identity);
